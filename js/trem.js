83ce51023988b63f2560223639b1eaa396657a60
tota113 <105284820+tota113@users.noreply.github.com>
2022-05-18 09:29:58 -0300

    Add files via upload

--- js/trem.js
+++ js/trem.js
@@ -1,7 +1,37 @@
-function mudartexto() {
-    //modificando o botão via ID//
-    document.getElementById("botao").innerHTML = "botao do polveiro"
-    alert("vitor skin head do caralho")
-}
-
+// pego os elementos do li
+var btn = document.querySelectorAll(".key li"),
+    input = document.querySelector(".input-valor"),
+    operador = document.querySelectorAll(".operator");
+    
+    //percorro a lista para ver e comparar
+    //se as teclas foram clicadas.
+for(var i = 0; i < btn.length; ++){
+    document.onkeypress = function (event){
+         for(var e = 0; e <= 10; e++){
+             if(key === (48+e)){
+                 input.innerHTML += e;
+             }
+         }
+        switch(key){
+             case 42:
+                 input.innerHTML += "*";
+                 break;
+             case 43:
+                   inputu.innerHTML += "+";
+                   break;
+             case 45:
+                 input.innerHTML += "-";
+                   break;
+             case 46:
+                 input.innerHTML += ".";
+                   break;
+             case 47
+                 input.innerHTML += "/";
+                   break;
+             case 13:
+             case 61:
+                   vae equacao = input.innerHTML
+        }
+        }
+};
 
